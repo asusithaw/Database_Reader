4a74b19257aba7a08949d97e42864c4357ec9319
asusithaw <asusithaw@gmail.com>
2023-08-25 18:50:29 +0530

    first commit - Reading Database Queries

---
+++ Database_Reader/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Database_Reader;
+using System.Timers;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var timer = new System.Timers.Timer(2000);
+        timer.Elapsed += OnEventExecution;
+        timer.Start();       
+    }
+
+    public static void OnEventExecution(Object? sender, ElapsedEventArgs eventArgs)
+    {
+        ReadingDb.MySqlReader();
+        
+    }
+}
+
+
+        
---
+++ Database_Reader/ReadingDb.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+namespace Database_Reader
+{
+    public class ReadingDb
+    {        
+        public static void MySqlReader()
+        {
+            string connectionString = "datasource=localhost;port=3306;username=root;password=Susitha@1997;";
+            string output = @"D:\\ASP.net - Winforms\\output.txt";
+            string currentDateTime = DateTime.Now.ToString();
+            var sqlQueries = new string[] { "SHOW STATUS LIKE '%lock%';", "SHOW STATUS LIKE '%current_waits%';", "SHOW GLOBAL STATUS;" };
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                foreach (string sql in sqlQueries)
+                {
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                        {
+                            string allData = "";
+                            string caption = $"The output for the sql query: {sql} & Time Stamp is {currentDateTime}";
+
+                            while (reader.Read())
+                            {
+                                string value = reader.GetString("Value");
+                                string variableName = reader.GetString("Variable_name");
+
+                                string line = $"{value}          {variableName}";
+                                allData += line + Environment.NewLine;
+                            }
+                            allData = caption + Environment.NewLine + "Value   " + "Variable Name" + Environment.NewLine + allData;
+                            File.AppendAllText(output, allData);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Data has been written to the file at {currentDateTime}");
+        }
+    }
+}
